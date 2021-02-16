9489cf5ac1a3f964bd8ef0c4eaeab69c790ce155
fastrgv <fastrgv@gmail.com>
2021-02-16 13:33:27 -0800

    Update wcmp0.bat

--- adaOpenAL/cppExample/wcmp0.bat
+++ adaOpenAL/cppExample/wcmp0.bat
@@ -3,14 +3,14 @@ del *.o
 del *.ali
 
 rem create snd4ada.o:
-c:\gnat64\2020\bin\gnatmake ^
+gnatmake ^
 	-c ../snd4ada -I.. ^
 	-I../OalBinding/incoal ^
 	-I../OalBinding
 
 
 rem create oal.o:
-c:\gnat64\2020\bin\g++ ^
+g++ ^
 	-c ../OalBinding/oal.cpp ^
 	-I.. -I../OalBinding -I../OalBinding/incoal
 
